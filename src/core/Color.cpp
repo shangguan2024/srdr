d080d816a525049c7b4cf4baee9837ff13c329ee
sg <1478624641@qq.com>
2026-04-20 17:43:11 +0800

    feat(Color): 添加颜色分量访问和打包方法
    添加 red()、green()、blue()、alpha() 成员函数以获取颜色分量值，并添加 toUint32() 方法将颜色打包为 #AARRGGBB 格式的 32 位无符号整数，便于颜色数据的处理和交换。

--- src/core/Color.cpp
+++ src/core/Color.cpp
@@ -1,3 +1,24 @@
 #include "Color.hpp"
+#include <cmath>
+#include <cstdint>
 
-namespace srdr {}
+namespace srdr {
+
+uint8_t Color::red() const { return uint8_t(std::round(m_data[0] * 255)); }
+
+uint8_t Color::green() const { return uint8_t(std::round(m_data[1] * 255)); }
+
+uint8_t Color::blue() const { return uint8_t(std::round(m_data[2] * 255)); }
+
+uint8_t Color::alpha() const { return uint8_t(m_data[3] * 255); }
+
+uint32_t Color::toUint32() const {
+    uint32_t val;
+    ((uint8_t*) (&val))[3] = alpha();
+    ((uint8_t*) (&val))[2] = red();
+    ((uint8_t*) (&val))[1] = green();
+    ((uint8_t*) (&val))[0] = blue();
+    return val;
+}
+
+} // namespace srdr
--- src/core/Color.hpp
+++ src/core/Color.hpp
@@ -2,12 +2,21 @@
 #define COLOR_HPP
 
 #include "Vector.hpp"
+#include <cstdint>
 
 namespace srdr {
 
 class Color {
 public:
     Color() = default;
+
+    uint8_t red() const;
+    uint8_t green() const;
+    uint8_t blue() const;
+    uint8_t alpha() const;
+
+    // Returns a 4 byte value equal to #AARRGGBB
+    uint32_t toUint32() const;
 
 private:
     Vec4f m_data;
