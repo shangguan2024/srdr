402d96e71e8652a9f014fd0afecf30d34f8643f7
sg <1478624641@qq.com>
2026-04-21 00:41:04 +0800

    feat(platform): 添加 Win32Window 类的接口和成员变量
    添加 Win32Window 类的析构函数和多个成员方法声明，包括窗口销毁、运行状态检查、帧绘制和窗口过程回调。同时添加必要的成员变量以支持后续实现。

--- src/platform/Win32Window.cpp
+++ src/platform/Win32Window.cpp
@@ -1,10 +1,33 @@
 #include "Win32Window.hpp"
+#include <cstdint>
 
 namespace srdr {
+
+Win32Window::~Win32Window() {
+    // TODO
+}
 
 bool Win32Window::create(int width, int height, const char* title) {
     // TODO
     return false;
+}
+
+void Win32Window::destroy() {
+    // TODO
+}
+
+bool Win32Window::isRunning() {
+    // TODO
+    return false;
+}
+
+void Win32Window::drawFrame(const uint32_t* pixels) {
+    // TODO
+}
+
+LRESULT CALLBACK Win32Window::WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    // TODO
+    return 0ll;
 }
 
 } // namespace srdr
--- src/platform/Win32Window.hpp
+++ src/platform/Win32Window.hpp
@@ -2,17 +2,31 @@
 #define WIN32WINDOW_HPP
 
 #include "IWindow.hpp"
+#include <cstdint>
+#include <windows.h>
 
 namespace srdr {
 
 class Win32Window : public IWindow {
 public:
-    Win32Window();
+    Win32Window() = default;
     ~Win32Window();
 
     bool create(int width, int height, const char* title) override;
+    void destroy() override;
+
+    bool isRunning() override;
+    void drawFrame(const uint32_t* pixels) override;
 
 private:
+    static LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+    HWND m_hWnd = nullptr;
+    HDC m_hdcMem = nullptr;
+    HBITMAP m_hBitmap = nullptr;
+    uint32_t* m_pixels = nullptr;
+    int m_width = 0;
+    int m_height = 0;
 };
 
 } // namespace srdr
