e1bb4583113343450a6ac8244f38f01ab098ef70
sg <1478624641@qq.com>
2026-04-28 02:15:45 +0800

    feat(renderer): 实现输出合并器的深度测试和混合功能
    添加深度测试开关，支持基于深度缓冲的片段剔除。添加混合开关和混合函数配置，支持自定义颜色混合逻辑。默认使用 alpha 混合公式：out = src * src.a + dst * (1.0 - src.a)。

--- src/renderer/OutputMerger.cpp
+++ src/renderer/OutputMerger.cpp
@@ -1,13 +1,47 @@
 #include "OutputMerger.hpp"
+#include "Color.hpp"
 
 namespace srdr {
 
+static BlendFunction s_default_blend_function = [](const Color& src, const Color& dst) {
+    Color out;
+    out = src * src.a + dst * (1.0f - src.a);
+    return out;
+};
+
+OutputMerger::OutputMerger()
+        : m_blend_function(s_default_blend_function) {}
+
 void OutputMerger::mergeOutputs(const std::vector<FragmentOutput>& outputs, FrameBuffer* fb) {
-    // TODO: z ...
     for (const auto& pixel: outputs) {
         const auto& [x, y, z] = pixel.o_position;
-        fb->writeColor(0, x, y, pixel.o_color);
+        auto writeColor = [&]() {
+            if (m_enable_blend) {
+                fb->writeColor(0, x, y, m_blend_function(pixel.o_color, fb->getColor(0, x, y)));
+            } else {
+                fb->writeColor(0, x, y, pixel.o_color);
+            }
+        };
+        if (m_enable_depth_test) {
+            if (fb->testAndWriteDepth(x, y, z)) {
+                writeColor();
+            }
+        } else {
+            writeColor();
+        }
     }
 }
+
+void OutputMerger::enableDepthTest() { m_enable_depth_test = true; }
+
+void OutputMerger::disableDepthTest() { m_enable_depth_test = false; }
+
+void OutputMerger::enableBlend() { m_enable_blend = true; }
+
+void OutputMerger::disableBlend() { m_enable_blend = false; }
+
+void OutputMerger::setBlendFunction(BlendFunction blend_func) { m_blend_function = blend_func; }
+
+void OutputMerger::resetBlendFunction() { m_blend_function = s_default_blend_function; }
 
 } // namespace srdr
--- src/renderer/OutputMerger.hpp
+++ src/renderer/OutputMerger.hpp
@@ -3,15 +3,33 @@
 
 #include "Fragment.hpp"
 #include "FrameBuffer.hpp"
+#include <functional>
 #include <vector>
 
 namespace srdr {
 
+using BlendFunction = std::function<Color(const Color&, const Color&)>;
+
 class OutputMerger {
 public:
+    OutputMerger();
+
     void mergeOutputs(const std::vector<FragmentOutput>& outputs, FrameBuffer* fb);
 
-    // TODO
+    void enableDepthTest();
+    void disableDepthTest();
+
+    void enableBlend();
+    void disableBlend();
+
+    void setBlendFunction(BlendFunction blend_func);
+    void resetBlendFunction();
+
+private:
+    bool m_enable_depth_test;
+    bool m_enable_blend;
+
+    BlendFunction m_blend_function;
 };
 
 } // namespace srdr
